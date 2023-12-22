ed6d02e42d1e9b068a9016a545fcccf8bc412a39
Mehrshad <mehrshadmhmdii@gmail.com>
2023-12-22 12:09:14 -0800

    Routing for pages added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-//import LoginSignup from './Components/LoginSignup/LoginSignup';
-/***/
-//import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import MusicApp from "./Components/HomePage/MusicApp";
-//import Login from './Login';
 import { useDataLayerValue } from "./DataLayer";
-/***/
+import LoginSignUp from "./Components/LoginSignup/LoginSignup";
+import ProfilePage from "./Components/ProfilePage/ProfilePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-        {/*<LoginSignup/>*/}
-        <MusicApp/>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<MusicApp />} />
+          <Route path="/" element={<LoginSignUp />} />
+          <Route path="/Profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
