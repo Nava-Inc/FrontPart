4be3eefdb41b3ac80f9defc7da7a66874451eb24
Mehrshad <mehrshadmhmdii@gmail.com>
2023-12-26 19:00:59 -0800

    Artist page and profile page structure created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MusicApp from "./Components/HomePage/MusicApp";
 import { useDataLayerValue } from "./DataLayer";
 import LoginSignUp from "./Components/LoginSignup/LoginSignup";
 import ProfilePage from "./Components/ProfilePage/ProfilePage";
+import ArtistPage from "./Components/ArtistPage/ArtistPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/Home" element={<MusicApp />} />
           <Route path="/" element={<LoginSignUp />} />
           <Route path="/Profile" element={<ProfilePage />} />
+          <Route path="/Artist" element={<ArtistPage />} />
         </Routes>
       </Router>
     </div>
--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -2,16 +2,25 @@ import { Container } from "@mui/material";
 import AppBar from "../Utils/AppBar";
 import MusicCard from "../Utils/MusicCard";
 import { MyDrawer } from "../Utils/MyDrawer";
+import SongCard from "../HomePage/SongCard";
 import UploadButton from "../Utils/UploadButton";
+import { SongList } from "../Utils/SongList/SongList";
+import Brad1 from "../Assets/Brad1.jpg";
 import React from "react";
-import "../ProfilePage/ProfilePage.css";
+import "./ProfilePage.css";
 
 function ProfilePage() {
   return (
     <div>
       <AppBar />
       <Container className="ProfileContainer">
-        <div className="ProfileBody">ProfilePage</div>
+        <div className="ProfileBody">
+          <img className="ArtistImage" src={Brad1} alt="Artist Image" />
+          <div className="ArtistInfo">
+            <div className="ArtistName">Profile Name </div>
+            <div className="ArtistDescription">Profile Description</div>
+          </div>
+        </div>
 
         <UploadButton />
       </Container>
